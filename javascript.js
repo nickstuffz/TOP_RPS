034622f55b5fb6611385fdda2fd8dd4a01709c55
nickstuffz <nicholasng62@gmail.com>
2023-04-29 00:45:12 -0700

    Rework to move gameplay from console to actual webpage

--- javascript.js
+++ javascript.js
@@ -1,104 +1,99 @@
+let pScore = 0;
+let compScore = 0;
+let i = 0;
+const buttons = document.querySelectorAll('button');
+buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+        let pChoice = button.innerHTML;
+        let choice = pChoice.toLowerCase();
+        let result = runRPS(choice);
 
-
-
-function getComputerChoice() {
-    // randomly return "rock" "paper" or "scissors"
-    const options = ["rock", "paper", "scissors"];
-    return options[Math.floor(Math.random()*3)]
-}
-
-// function that plays 1 round of RPS
-// 2 parameters, case insensitive
-
-function runRPS(pSelect) {
-
-    compSelect = getComputerChoice();
-    console.log("The computer chose " + compSelect);
-
-    if (!(pSelect === "rock" || pSelect === "paper" || pSelect === "scissors")) {
-        return "Input error, not RPS material. You fool try again"
-    }
-    else if (pSelect === compSelect) {
-        return "Stalemate Baby!"
-    }
-    else if (pSelect === "rock") {
-        if (compSelect === "paper") {
-            return "You lose loser!"
-        }
-        else {
-            return "Winner winner chicken dinner!"
-        }
-    }
-    else if (pSelect === "paper") {
-        if (compSelect === "scissors") {
-            return "You lose loser!"
-        }
-        else {
-            return "Winner winner chicken dinner!"
-        }
-    }
-    else if (pSelect === "scissors") {
-        if (compSelect === "rock") {
-            return "You lose loser!"
-        }
-        else {
-            return "Winner winner chicken dinner!"
-        }
-    }
-    else {
-        return "Something went wrong sorry!"
-    }
-}
-
-// let choice = prompt("Type your choice!").toLowerCase();
-// console.log("You chose " + choice);
-// console.log(runRPS(choice));
-
-// console.log(runRPS("paper"))
-
-function game() {
-    alert("This is an RPS versus ur puter, BO5, to the death!")
-    let pScore = 0;
-    let compScore = 0;
-
-    for (let i=1; i<6; i++) {
-        let pChoice = prompt("Type your choice!");
-
-        if (pChoice === null) {
-            console.log("You scared? I don't blame you")
-            return;
-        }
-
-        choice = pChoice.toLowerCase();
-        console.log("You chose " + choice);
-        result = runRPS(choice);
-        console.log(result);
-
-        if (result === "Input error, not RPS material. You fool try again") {
-            i -= 1
-            continue;
-        }
-        else if (result === "Winner winner chicken dinner!") {
+        i += 1;
+        if (result === "Winner winner chicken dinner!") {
             pScore += 1;
         }
         else if (result === "You lose loser!") {
             compScore += 1;
         }
         
-        console.log("After round " + i + " total score:")
-        console.log("You = " + pScore, "puter = " + compScore)
-    }
+        const container = document.querySelector('#container');
+        const p1 = document.createElement('p');
+        p1.textContent = ("You chose " + choice +
+        ", the computer chose " + compSelect + ".");
+        container.appendChild(p1);
+        
+        const p2 = document.createElement('p');
+        p2.textContent = (result);
+        container.appendChild(p2);
 
-    if (pScore > compScore) {
-        return console.log("You have won, you may now smash your puter!")
+        const p3 = document.createElement('p');
+        p3.textContent = ("After round " + i + " the total score: " +
+        "YOU = " + pScore + ", ur puter = " + compScore + ".");
+        container.appendChild(p3);
+
+        if (i === 5) {
+            if (pScore > compScore) {
+                final = "You have won, you may now smash your puter!";
+            }
+            else if (compScore > pScore) {
+                final = "Your puter now owns you, legally.";
+            }
+            else {
+                final = "You tied, you may live in peace... for now."; 
+            }
+        const fscore = document.createElement('h3');
+        fscore.textContent = (final);
+        container.appendChild(fscore);              
+        
+        // kill the buttons
+        const body = document.querySelector('body');
+        body.removeChild(document.querySelector('#selector'));
+        }
+    });
+});
+
+
+
+function getComputerChoice() {
+    const options = ["rock", "paper", "scissors"];
+    return options[Math.floor(Math.random()*3)];
+    // randomly returns "rock" "paper" or "scissors"
+}
+
+function runRPS(pSelect) {
+
+    compSelect = getComputerChoice();
+
+    if (pSelect === compSelect) {
+        return "Stalemate Baby!";
     }
-    else if (compScore > pScore) {
-        return console.log("Your puter now owns you, legally.")
+    else if (pSelect === "rock") {
+        if (compSelect === "paper") {
+            return "You lose loser!";
+        }
+        else {
+            return "Winner winner chicken dinner!";
+        }
+    }
+    else if (pSelect === "paper") {
+        if (compSelect === "scissors") {
+            return "You lose loser!";
+        }
+        else {
+            return "Winner winner chicken dinner!";
+        }
+    }
+    else if (pSelect === "scissors") {
+        if (compSelect === "rock") {
+            return "You lose loser!";
+        }
+        else {
+            return "Winner winner chicken dinner!";
+        }
     }
     else {
-        return console.log("You tied, you may live in peace... for now.")
+        return "Something went wrong sorry!";
     }
 }
 
-game()
 
